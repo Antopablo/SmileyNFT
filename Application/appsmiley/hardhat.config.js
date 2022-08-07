6d1aca601e546b7042a255583b4621a6419cba21
Antopablo <lefevreanto57@gmail.com>
2022-08-07 15:53:09 +0200

    first compile hardhat

--- Application/appsmiley/hardhat.config.js
+++ Application/appsmiley/hardhat.config.js
@@ -2,5 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.12",
+  paths: {
+    artifacts: './artifacts'
+  },
+  networks: {
+    hardhat: {
+      chainId: 1377
+    }
+  }
 };
