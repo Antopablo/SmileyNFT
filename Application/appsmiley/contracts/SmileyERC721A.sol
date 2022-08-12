fdcc2cc61227a5dcc3f6a6d98e88e69aafa560b2
Antopablo <lefevreanto57@gmail.com>
2022-08-12 14:29:44 +0200

    Constructors + bases functions

--- Application/appsmiley/contracts/SmileyERC721A.sol
+++ Application/appsmiley/contracts/SmileyERC721A.sol
@@ -8,5 +8,132 @@ import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "./ERC721A.sol";
+import "./ERC721AQueryable.sol";
 
-contract SmileyERC721A is Ownable, ERC712A, PaymentSplitter {}
+contract SmileyERC721A is Ownable, ERC721A, ERC721AQueryable, PaymentSplitter {
+    using Strings for uint256;
+
+    enum Step {
+        Before,
+        WhitelistSale,
+        PublicSale,
+        SoldOut,
+        Reveal
+    }
+
+    Step public sellingStep;
+
+    uint256 private constant MAX_SUPPLY = 30;
+    uint256 private constant MAX_GIFT = 5;
+    uint256 private constant MAX_WHITELIST = 5;
+    uint256 private constant MAX_PUBLIC = 20;
+    uint256 private constant MAX_SUPPLY_MINUS_GIFT = MAX_SUPPLY - MAX_GIFT;
+
+    uint256 public wlSalePrice = 0.01 ether;
+    uint256 public publicSalePrice = 0.03 ether;
+
+    uint256 public saleStartTime = 1661983201;
+
+    bytes32 public merkleRoot;
+
+    string public baseURI;
+
+    mapping(address => uint256) amountNFTperWalletWhiteListSale;
+    mapping(address => uint256) amountNFTperWalletPublicSale;
+
+    uint256 private constant MAX_PER_ADRESS_DURING_WHITELIST_MINT = 1;
+    uint256 private constant MAX_PER_ADRESS_DURING_PUBLIC_MINT = 3;
+
+    bool public isPaused;
+
+    uint256 private teamLength;
+
+    address[] private _team = [
+        0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266,
+        0x70997970C51812dc3A010C7d01b50e0d17dc79C8
+    ];
+
+    uint256[] private _teamShares = [700, 300];
+
+    constructor(bytes32 _merkleRoot, string memory _baseURI)
+        ERC721A("Smiley", "SMY")
+        PaymentSplitter(_team, _teamShares)
+    {
+        merkleRoot = _merkleRoot;
+        baseURI = _baseURI;
+        teamLength = _team.length;
+    }
+
+    /**
+     * @notice Change whitelist sale price
+     *
+     * @param _wlSalePrice The new price
+     */
+    function setWlSalePrice(uint256 _wlSalePrice) external onlyOwner {
+        wlSalePrice = _wlSalePrice;
+    }
+
+    /**
+     * @notice Change public sale price
+     *
+     * @param _publicSalePrice The new price
+     */
+    function setPublicSalePrice(uint256 _publicSalePrice) external onlyOwner {
+        publicSalePrice = _publicSalePrice;
+    }
+
+    /**
+     * @notice Change start time
+     *
+     * @param _saleStartTime The new timestamp
+     */
+    function setSaleStartTime(uint256 _saleStartTime) external onlyOwner {
+        saleStartTime = _saleStartTime;
+    }
+
+    /**
+     * @notice Return current timestamp
+     *
+     * @return currentTimeStamp The current timestamp
+     */
+    function currentTime() internal view returns (uint256) {
+        return block.timestamp;
+    }
+
+    /**
+     * @notice Change the step of the sale
+     *
+     * @param _step The new step
+     */
+    function setStep(Step _step) external onlyOwner {
+        // mettre uint et convertir _step en Step(_step)
+        sellingStep = _step;
+    }
+
+    /**
+     * @notice Change the state of isPaused
+     *
+     * @param _isPaused state
+     */
+    function setPause(bool _isPaused) external onlyOwner {
+        isPaused = _isPaused;
+    }
+
+    /**
+     * @notice Change the baseURI
+     *
+     * @param _baseURI new baseURI
+     */
+    function setBaseURI(string memory _baseURI) external onlyOwner {
+        baseURI = _baseURI;
+    }
+
+    /**
+     * @notice Change the merkleRoot
+     *
+     * @param _merkleRoot new merkleRoot
+     */
+    function setMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
+        merkleRoot = _merkleRoot;
+    }
+}
