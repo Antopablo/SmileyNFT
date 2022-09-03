043f939f0f334965dd5dae61d9d67e950814da29
Antopablo <lefevreanto57@gmail.com>
2022-09-03 16:29:48 +0200

    First Deploy

--- Application/appsmiley/scripts/deploy.js
+++ Application/appsmiley/scripts/deploy.js
@@ -1,29 +1,33 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
+const { MerkleTree } = require('merkletreejs');
+const keccak256 = require("keccak256");
+const tokens = require('../tokens.json');
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  let tab = [];
+  tokens.map((token) => {
+    tab.push(token.address);
+  });
+  const leaves = tab.map((address) => keccak256(address));
+  const tree = new MerkleTree(leaves, keccak256, { sort: true });
+  const root = tree.getHexRoot();
+  const baseURI = "ipfs://QmW7XqjS1AaNTmhqvhqJLfe1fyiGNXMVPgUdw7aYTncwmd/"
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const Contract = await hre.ethers.getContractFactory("SmileyERC721A");
+  const contract = await Contract.deploy(root, baseURI);
 
-  await lock.deployed();
+  await contract.deployed();
 
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log("Deployed to : " + contract.address, " - root: " + root + " - baseURI: " + baseURI);
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
