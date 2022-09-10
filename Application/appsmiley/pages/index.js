aeccfef07dc180f987c029bc0225a4a67831ebb1
Antopablo <lefevreanto57@gmail.com>
2022-09-10 15:50:10 +0200

    add import in index

--- Application/appsmiley/pages/index.js
+++ Application/appsmiley/pages/index.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react"
-import { Flex } from "@chakra-ui/react"
-import Layout from "../components/Layout/Layout"
+import React, { useState, useEffect } from "react";
+import { Flex, Button, Spinner, Text, useToast } from "@chakra-ui/react";
+import Layout from "../components/Layout/Layout";
+import useEthersProvider from "../hooks/useEthersProvider";
 
 export default function Home() {
   return (
